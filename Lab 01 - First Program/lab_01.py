93b2dc088879c25a3b9efb538165d02c29c82bf8
Jaiirog <jairogonzalezdeoro@gmail.com>
2022-02-10 18:38:50 +0100

    acabao

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,24 @@
 Hi
 
-asas
-opop
+
+
+"__COMANDOS__"
+
+"Git status---------See what has changed
+"Git fetch---------Grab stuff from the server, but don’t
+"Merge git merge --no-commit--no-ff test_branch---------Merge
+"Git merge --abort------Abort a merge
+"Git pull--------Fetch and Merge
+"Git add myfile.txt--------Add myfile.txt to be committed
+"Git add . ---------Add everything
+"Grep -r<< HEAD *-----------Search all files to see if there is merge error text.Do this before committing
+"Git checkout - -ours "myfile.txt"--------Toss your changes in a merge, use theirs
+"Git checkout - -theirs "myfile.txt"----------Toss their changes, use yours " \
+"Git checkout - -.Remove all your changes, go back to what was last committed.Untracked files are kept.
+"Git - f clean----------------Remove untracked files
+"Git checkout 44fd-------Find the hash of a check - in, and you can go back to that check in." \
+"(Don’t use 44fd, but replace with the has you want.)
+"Git checkout master------------Go back to most recent check in on the master branch.
+"Git commit -m "My message"----------------Commit your work.Use a descriptive message or the other people in the" \
+                          "class will be irritated with you." \
+"git push------------Push commit up to the serve git status
